5bfca3d4dcf0cfddf7b0f6bf3b1f761191237d17
AntOnAntOff <povanton@gmail.com>
2023-08-19 17:27:53 +0300

    added message error for method PATCH

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -43,6 +43,7 @@ const removeContact = async (req, res) => {
 
 const updateFavorite = async (req, res) => {
   const { contactId } = req.params;
+
   const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
